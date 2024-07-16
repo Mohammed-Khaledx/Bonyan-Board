53844fbbab97d5458141ade0f5ad25f601eabc0e
Mohammed-Khaledx <mohammedkhaledx1@gmail.com>
2024-07-16 14:42:45 +0300

    Bug fixed

--- bonyan.js
+++ bonyan.js
@@ -1,3 +1,5 @@
+//*************************************************************** */
+// Query selectors for different button types and lists
 const addBtns = document.querySelectorAll(".add");
 let deleteBtns = document.querySelectorAll(".delete");
 let updateBtns = document.querySelectorAll(".update");
@@ -5,58 +7,38 @@ const upComingList = document.querySelectorAll(".up-coming");
 const inProgressList = document.querySelectorAll(".in-Progress");
 const finishedList = document.querySelectorAll(".finished-l");
 
-// empty();
-
+// Task counters for each column
 let taskCounter = {
   "up-Coming": 0,
   "in-Progress": 0,
   "finished-l": 0,
 };
 
-// set the task to be global
-let storedTasks = JSON.parse(localStorage.getItem("tasks"))
-  ? JSON.parse(localStorage.getItem("tasks"))
-  : [];
-
-
+// Retrieve stored tasks from localStorage or initialize as an empty array
+let storedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
 console.log(storedTasks);
-let upcoming = storedTasks.filter((task) => task.parent === "up-Coming");
-// onStarting or on reloading the page the page we should load all tasks from local storage
-//* loadTasks();
+
+// Load tasks from localStorage when the page loads
 loadStorage();
 
-// then we should implement this
-// loadTask should query all the task which is already exist
-// the existence of the tasks came from storeTask() function or deleteTask() function
-//* storeTask() , deleteTask()
-// the add task function should store the task in its column
-// storetask() should be called wen ever add btn clicked
-// or when ever the drag,drop finished as this is mean new tasks for every column
-// delete task() should be called when delete btn clicked and when drag,drop finished
-
-// console.log(taskCounter["up-coming"] + 1+1)
-
-// setting up the add items btn
+// Add event listeners to add buttons
 addBtns.forEach((addBtn) => {
-  // deleteBtns = document.querySelectorAll(".delete");
   addBtn.addEventListener("click", function createItem(e) {
     let columnName = this.parentNode.childNodes[3].className.split(" ")[1];
-    let content = "task" + taskCounter[columnName];
+    let content = "Task " + taskCounter[columnName];
     let taskId = Date.now();
 
-    // the create create and attach delete listener
     createItemElement(taskId, content, columnName);
-
-    taskCounter[e.target.parentNode.childNodes[3].className.split(" ")[1]] += 1;
+    taskCounter[columnName] += 1;
 
     storeTask(taskId, content, columnName);
     attachDragDrop();
   });
 });
 
+// Attach drag and drop event listeners to items and drop zones
 function attachDragDrop() {
   let items = document.querySelectorAll(".item");
-
   items.forEach((item) => {
     item.addEventListener("dragstart", handleDragStart);
     item.addEventListener("dragover", handleDragOver);
@@ -64,35 +46,21 @@ function attachDragDrop() {
     item.addEventListener("dragleave", handleDragLeave);
     item.addEventListener("dragend", (e) => {
       handleDragEnd(e.target);
-      items.forEach((item) => {
-        item.classList.remove("over");
-      });
+      items.forEach((item) => item.classList.remove("over"));
     });
-
     item.addEventListener("drop", handleDrop);
+  });
+
+  let zones = document.querySelectorAll(".dropZone");
+  zones.forEach((list) => {
+    list.addEventListener("dragenter", handleEmptyDragEnter);
+    list.addEventListener("dragleave", handleEmptyDragLeave);
+    list.addEventListener("dragover", handleDragOver);
+    list.addEventListener("drop", handleEmptyDrop);
   });
 }
 
-// function empty() {
-//   let lists = document.querySelectorAll("h2");
-
-//   lists.forEach((list) => {
-//     // item.addEventListener("dragstart", handleDragStart);
-//     // item.addEventListener("dragover", handleDragOver);
-//     list.addEventListener("dragenter", handleEmptyDragEnter);
-//     list.addEventListener("dragleave", handleEmptyDragLeave);
-//     list.addEventListener("dragend", (e) => {
-//       handleDragEnd(e.target);
-//       list.forEach((list) => {
-//         list.classList.remove("over");
-//       });
-//     });
-
-//     list.addEventListener("drop", handleDrop);
-//   });
-// }
-
-let draggedEl = document.createElement("div");
+// Drag and Drop Event Handlers
 function handleDragStart(e) {
   this.style.opacity = "0.3";
   e.dataTransfer.effectAllowed = "move";
@@ -116,52 +84,50 @@ function handleDragLeave(e) {
   this.classList.remove("over");
 }
 
-// function handleEmptyDragEnter(e) {
-//   this.style.opacity = "0";
-// }
+function handleEmptyDragEnter(e) {
+  this.classList.add("over");
+}
 
-// function handleEmptyDragLeave(e) {
-//   this.style.border = "1";
-// }
+function handleEmptyDragLeave(e) {
+  this.classList.remove("over");
+}
 
-function handleDrop(e) {
+function handleEmptyDrop(e) {
   e.stopPropagation();
-  console.log(draggedEl);
-  if (draggedEl !== this || draggedEl !== undefined) {
-    // draggedEl.innerHTML = this;
-    let newColumn = this.parentNode.className.split(" ")[1];
-    let droppedId = e.dataTransfer.getData("text/html");
-    let secret = document.getElementById(droppedId);
-    this.after(secret);
+  let newColumn = this.parentNode.className.split(" ")[1];
+  let droppedId = e.dataTransfer.getData("text/html");
+  let taskElement = document.getElementById(droppedId);
+  this.after(taskElement);
+  this.classList.remove("over");
 
-    changeStoredColumn(droppedId, newColumn);
-  }
-  attachDragDrop();
+  changeStoredColumn(droppedId, newColumn);
   return false;
 }
 
-// !!!!!!  ID should be unique
-// const taskId = Date.now(); //:)ULTRA Unique ID for each task
+function handleDrop(e) {
+  e.stopPropagation();
+  let newColumn = this.parentNode.className.split(" ")[1];
+  let droppedId = e.dataTransfer.getData("text/html");
+  let taskElement = document.getElementById(droppedId);
+  this.after(taskElement);
 
-//? local storage logic:
-// // get the items form storage as an array of objects
-// let tasks = JSON.parse(localStorage.getItem("tasks")) || [];
-// // make the array of object available to be stored in local storage
-// localStorage.setItem("tasks", JSON.stringify(tasks));
-// //filter the deleted tasks
-// tasks = tasks.filter((task) => task.id !== taskId);
+  changeStoredColumn(droppedId, newColumn);
+  attachDragDrop();
 
+  return false;
+}
+
+// Task management functions
 function storeTask(id, content, columnName) {
   const newTask = { id: id, title: content, parent: columnName };
   storedTasks.push(newTask);
   localStorage.setItem("tasks", JSON.stringify(storedTasks));
 }
+
 function updateTask(id, content) {
-  // let updatedTask = storedTasks.filter((task) => task.id == id);
   storedTasks.forEach((task) => {
     if (task.id == id) {
       task.title = content;
-      console.log(true);
       localStorage.setItem("tasks", JSON.stringify(storedTasks));
       return;
     }
@@ -170,8 +136,6 @@ function updateTask(id, content) {
 
 function removeTask(id) {
   storedTasks = storedTasks.filter((task) => task.id != id);
-  console.log(id);
-  console.log(storedTasks);
   localStorage.setItem("tasks", JSON.stringify(storedTasks));
 }
 
@@ -184,6 +148,7 @@ function changeStoredColumn(id, columnName) {
   localStorage.setItem("tasks", JSON.stringify(storedTasks));
 }
 
+// Load tasks from localStorage into their respective columns
 function loadStorage() {
   let upcomingTasks = storedTasks.filter((task) => task.parent === "up-Coming");
   let inProgressTasks = storedTasks.filter(
@@ -193,19 +158,20 @@ function loadStorage() {
     (task) => task.parent === "finished-l"
   );
 
-  upcomingTasks.forEach((task) => {
-    createItemElement(task.id, task.title, task.parent);
-  });
-  inProgressTasks.forEach((task) => {
-    createItemElement(task.id, task.title, task.parent);
-  });
-  finishedTasks.forEach((task) => {
-    createItemElement(task.id, task.title, task.parent);
-  });
+  upcomingTasks.forEach((task) =>
+    createItemElement(task.id, task.title, task.parent)
+  );
+  inProgressTasks.forEach((task) =>
+    createItemElement(task.id, task.title, task.parent)
+  );
+  finishedTasks.forEach((task) =>
+    createItemElement(task.id, task.title, task.parent)
+  );
 
   attachDragDrop();
 }
 
+// Create task elements and attach them to the DOM
 function createItemElement(id, content, columnName) {
   let column = document.querySelector("." + columnName);
   let item = document.createElement("div");
@@ -213,37 +179,35 @@ function createItemElement(id, content, columnName) {
   item.id = id;
   item.draggable = true;
 
-  item.innerHTML = `<input type="text" value = ${content} />
-  <button class="delete">D</button>
-  <button class="update">U</button>`;
+  item.innerHTML = `
+    <input type="text" value="${content}" />
+    <button class="delete">D</button>
+    <button class="update">U</button>
+  `;
 
   column.append(item);
   deleteItem();
   updateItem(id);
 }
 
+// Attach event listeners to delete buttons
 function deleteItem() {
   deleteBtns = document.querySelectorAll(".delete");
-  // setting up the Delete items btn
   deleteBtns.forEach((deleteBtn) => {
-    // deleteBtns = document.querySelectorAll(".delete");
     deleteBtn.addEventListener("click", (e) => {
-      // console.log(e.target.parentNode.id)
       removeTask(e.target.parentNode.id);
       e.target.parentNode.remove();
     });
   });
 }
 
+// Attach event listeners to update buttons
 function updateItem(id) {
   updateBtns = document.querySelectorAll(".update");
   updateBtns.forEach((updateBtn) => {
     updateBtn.addEventListener("click", (e) => {
-      console.log("??");
-      updateTask(
-        e.target.parentNode.id,
-        e.target.parentNode.childNodes[0].value.trim()
-      );
+      let content = e.target.parentNode.childNodes[1].value.trim();
+      updateTask(e.target.parentNode.id, content);
     });
   });
 }
